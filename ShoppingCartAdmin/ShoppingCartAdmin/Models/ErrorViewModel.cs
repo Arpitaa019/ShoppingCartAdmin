905f0bad9f1fc0617ea2ca4c131886b8993a039a
Arpita <jaiswalarpita109@gmail.com>
2025-10-26 22:23:15 +0530

    first commit

---
+++ ShoppingCartAdmin/ShoppingCartAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShoppingCartAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
